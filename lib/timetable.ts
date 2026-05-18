a387d4af158410d4f9ff75ca781765b67844e16d
정찬영 <rftgy1029@naver.com>
2026-05-18 17:04:27 +0900

    Remove sample timetable subjects

--- lib/timetable.ts
+++ lib/timetable.ts
@@ -1,9 +1,9 @@
 import type { Timetable } from '@/types/timetable';
 
 export const defaultTimetable: Timetable = {
-  monday: ['국어', '수학', '영어', '과학', '체육'],
-  tuesday: ['사회', '국어', '음악', '수학', '영어'],
-  wednesday: ['수학', '과학', '국어', '미술', '동아리'],
-  thursday: ['영어', '사회', '체육', '국어', '과학'],
-  friday: ['국어', '수학', '영어', '진로', '창체'],
+  monday: [],
+  tuesday: [],
+  wednesday: [],
+  thursday: [],
+  friday: [],
 };
